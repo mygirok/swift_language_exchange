27b9e7e1e0f069fc7e224ee0ff6d3346ff586d05
mygirok <forgirok@gmail.com>
2021-12-16 21:40:15 +0900

    Login screen layout

---
+++ LanguageExchange/Controllers/Login/RegisterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewController.swift
+//  RegisterViewController
+//
+//  Created by forworkyong on 2021/12/14.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        view.backgroundColor = .link
+        view.backgroundColor = .white
+    }
+}
